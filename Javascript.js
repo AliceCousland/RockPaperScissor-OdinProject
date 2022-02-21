b45eaa318c4ce01f7a8d9dfa3e6d278c09783039
AliceCousland <Sesabsoares@gmail.com>
2022-02-21 15:39:35 -0300

    Random array for computer decision

---
+++ Javascript.js
@@ -0,0 +1,6 @@
+let computerPlay = ["Rock", "Paper", "Scissors"];
+
+let random = Math.floor(Math.random() * computerPlay.length);
+console.log(random, computerPlay[random]);
+// computer's decision
+    
