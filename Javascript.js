a09fdfd89f9be3e749e9f477de2e68f5b532a943
AliceCousland <Sesabsoares@gmail.com>
2022-02-21 18:09:29 -0300

    if-else if 'rock' statements

--- Javascript.js
+++ Javascript.js
@@ -1,14 +1,19 @@
 let playerSelection = prompt('Rock, Paper, or Scissors?')
-    = ['rock', 'Rock', 'ROCK',
+    playerSelection ['rock', 'Rock', 'ROCK',
     'paper', 'Paper', 'PAPER',
     'scissor', 'Scissor', 'SCISSOR']; 
-    console.log(playerSelection)//Player input
+    console.log(playerSelection)//Player input log
 
 let computerPlay = ["Rock", "Paper", "Scissors"];
-
 let random = Math.floor(Math.random() * computerPlay.length);
 console.log(random, computerPlay[random]);
 // computer's decision for random element in array
 
-
-    
+computerPlay =='rock'
+if(playerSelection =='rock' || playerSelection =='Rock' || playerSelection =='ROCK') {
+    alert('The game is tied! Rock cannot destroy rock!');
+} else if (playerSelection =='paper' || playerSelection =='Paper' || playerSelection =='PAPER') {
+    alert('You have won! Paper constrains the power of rock!');
+} else if (playerSelection =='scissors' || playerSelection =='Scissors' || playerSelection =='SCISSORS') {
+    alert('You lose! Scissors are smashed by rocks!')
+}
