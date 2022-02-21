fdbdb74bfdc63df1946456d4e4401d8906339ec4
AliceCousland <Sesabsoares@gmail.com>
2022-02-21 16:41:10 -0300

    prompt player input

--- Javascript.js
+++ Javascript.js
@@ -1,6 +1,14 @@
+let playerSelection = prompt('Rock, Paper, or Scissors?')
+    = ['rock', 'Rock', 'ROCK',
+    'paper', 'Paper', 'PAPER',
+    'scissor', 'Scissor', 'SCISSOR']; 
+    console.log(playerSelection)//Player input
+
 let computerPlay = ["Rock", "Paper", "Scissors"];
 
 let random = Math.floor(Math.random() * computerPlay.length);
 console.log(random, computerPlay[random]);
-// computer's decision
+// computer's decision for random element in array
+
+
     
